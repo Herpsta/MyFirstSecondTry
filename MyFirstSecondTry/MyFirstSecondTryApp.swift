c63ed8b7d08539f72f3d7e7d1e5a717de3e7d084
Herpsta <Nostalgic124@live.com>
2024-02-26 20:32:28 -0700

    Initial Commit

---
+++ MyFirstSecondTry/MyFirstSecondTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSecondTryApp.swift
+//  MyFirstSecondTry
+//
+//  Created by Charlotte Lister on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSecondTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
